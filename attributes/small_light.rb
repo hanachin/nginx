33a9372cba62ad1a74ecd411be35f36129983df5
Seiei Higa <hanachin@gmail.com>
2013-10-02 22:26:36 +0900

    Fix ngx_small_light download url

--- attributes/small_light.rb
+++ attributes/small_light.rb
@@ -1,3 +1,3 @@
-default['nginx']['small_light']['url']      = "https://github.com/cubicdaiya/ngx_small_light/archive/v0.5.0.zip"
-default['nginx']['small_light']['checksum'] = "c4a9b31932c0e956b142b0a00a2fdd957fc76067"
+default['nginx']['small_light']['url']      = "https://github.com/cubicdaiya/ngx_small_light/tarball/v0.5.0"
+default['nginx']['small_light']['checksum'] = "3f7823902c0a97a8e5443c89a7aa1f86f3157f2b"
 default['nginx']['small_light']['setup_option'] = ""
